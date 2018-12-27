826d17d111bbf84050a4789fe85168164313e170
Isaac Marroqui <isaac@andalusele.com>
2018-12-27 14:14:10 +0100

    añadimos a la pagina index su html

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -1,5 +1,75 @@
 @extends('principal') <!-- Extiene de principal, que contiene el head, menu, footer -->
 
 @section('contenido') <!-- Aquí se va a mostrar el ranking de webs -->
-  <h1> Inicio </h1>
+  <!-- post item -->
+  <article class="theme_article instock">
+    <div class="post_thumb image_wrapper no_thumb" style="width: 320px;">
+      <a href='web.php?id='>
+        <div class="sc_slider sc_slider_flex">
+          <ul class="slides">
+            <li><img style="margin-top: 50px;padding:5px;" alt="" src=""></li>
+          </ul>
+        </div>
+      </a>
+      <span class="post_format theme_accent_bg icon-camera"></span>
+      <span class="post_category theme_accent_bg"></span>
+    </div>
+
+    <div class="post_content">
+      <div class="post_info post_info_top theme_info">
+        <span class="post_comments">
+          <a href="single-format-standard.html">
+            <span class="comments_icon theme_info icon-eye"></span>
+            <span class="comments_number"></span>
+          </a>
+        </span>
+      </div>
+      <div class="title_area">
+        <h2 class="post_title"><a href='web.php?id='class="theme_title"></a></h2>
+        <!-- estrellas -->
+           <div class="reviews_summary blog_reviews">
+          <div class="criteria_summary criteria_row">
+            <span class="criteria_stars" title="4.3 from 5">
+              <span class="stars_off"><span class="theme_stars"></span><span class="theme_stars"></span><span class="theme_stars"></span><span class="theme_stars"></span><span class="theme_stars"></span></span>
+              <span class="stars_on" data-review=""><span class="theme_stars theme_stars_on"></span><span class="theme_stars theme_stars_on"></span><span class="theme_stars theme_stars_on"></span><span class="theme_stars theme_stars_on"></span><span class="theme_stars theme_stars_on"></span></span>
+            </span>
+          </div>
+        </div>
+        <!-- /estrellas -->
+         </div>
+
+      <!-- Info -->
+         <div class="post_info post_info_top theme_info">
+        <span class="post_cats">
+          <a class="cat_link" href="excerpt-style2.html">
+          </a>
+        </span>
+      </div>
+      <!-- /Info -->
+
+         <div class="post_text_area">
+        <div class="sc_column_item sc_column_item_4">
+          <ul class="sc_list sc_list_style_check">
+            <li class="sc_list_item first"><span class="sc_list_icon"></span></li>
+            <li class="sc_list_item"><span class="sc_list_icon"></span></li>
+            <li class="sc_list_item sc_list_style_error"><span class="sc_list_icon"></span></li>
+          </ul>
+        </div>
+        <a href="" class="more-link"> Visitar web</a>
+      </div>
+
+      <!-- Tags -->
+
+         <div class="post_info post_info_bottom theme_info" style="
+      border-bottom: 1px solid #00a8dd;margin-top:15px;"></div>
+
+      <span class="post_tags" style="vertical-align: -webkit-baseline-middle;display: -webkit-inline-box;margin-top: 10px;">
+        <span class="tags_label">Tags:</span>
+          <a class="tag_link" href="#">
+          </a>
+      </span>
+      <!-- /Tags -->
+       </div>
+  </article>
+  <!-- /post item -->
 @stop
--- resources/views/principal.blade.php
+++ resources/views/principal.blade.php
@@ -32,90 +32,106 @@
 </head>
 <body class="theme_regular puzzles_heavy reviews_5 puzzles_animations wide no-js" data-puzzlestyle="heavy">
 <div id="page" class="theme_body">
-		<header role="banner">
-			<div id="header_top">
-				<div class="top_line theme_accent_bg"></div>
-				<div id="header_top_inner">
-					<div id="login_area">
+	<header role="banner">
+		<div id="header_top">
+			<div class="top_line theme_accent_bg"></div>
+			<div id="header_top_inner">
 
-					<!-- Logo -->
-					<div class="logo logo_text">
-						<a href="{{ route ('inicio') }} ">
-							<span class="logo_title theme_header">Top<span class="theme_accent">web</span>meetings</span><span class="logo_slogan theme_info"></span>
-						</a>
-					</div>
-					<!-- /Logo -->
+				<!-- Area login -->
+				<div id="login_area">
+					<a href="#" class="link_login icon-login-1 theme_accent_bg" title="Login"></a>
+					<a href="#" class="link_register icon-key theme_accent_bg" title="Register"></a>
 				</div>
-   		</div>
-			<div id="header_middle_wrapper">
-				<div id="header_middle"  >
-					<div id="header_middle_inner">
-						<div class="search_form_area theme_body">
-							<form class="search_form" action="#" method="get"><input class="search_field" type="search" placeholder="Type your search query and press 'Enter' ..." value="" name="s"></form>
-							<a href="#" class="search_close"><span class="icon-cancel-circled"></span></a>
-						</div>
+				<!-- /Area Login -->
 
-						<!-- main menu -->
-						<nav id="mainmenu_area" class="mainmenu_area theme_menu" role="navigation">
-						<ul id="mainmenu" class="sf-js-enabled">
-							<li class="lightblue"><a href="{{ route ('inicio') }} " class="sf-with-ul">Inicio<span class="sf-sub-indicator icon-right-open"></span></a></li>
-							<li class="brown"><a href="{{ route ('web') }} " class="sf-with-ul">Webs<span class="sf-sub-indicator icon-right-open"></span></a></li>
-							<li id="blob" style="width: 0px; height: 0px; left: 0px; top: 0px; background-color: rgb(187, 187, 187); opacity: 1;"></li>
-							<!--<li class="red"><a href="editarweb" class="sf-with-ul"> Editar webs <span class="sf-sub-indicator icon-right-open"></span></a></li>-->
-						</ul>
-						<a href="#" class="search_link"><span class="icon-search"></span></a>
-						</nav>
-						<!-- #main menu -->
+				<!-- breadcrumbs -->
+				<div id="breadcrumbs_area">
+				  <ul class="breadcrumbs">
+				    <li class="home"><a href="index.php">Inicio</a></li>
+				    <li class="home"><a href="allwebs.php">Webs</a></li>
+				    <li class="home"><a href="blog.php">Blog</a></li>
+				  </ul>
+				</div>
+				<!-- /breadcrumbs -->
+ 		</div>
+		<div id="header_middle_wrapper">
+			<div id="header_middle"  >
+				<div id="header_middle_inner">
+					<div class="search_form_area theme_body">
+						<form class="search_form" action="#" method="get"><input class="search_field" type="search" placeholder="Type your search query and press 'Enter' ..." value="" name="s"></form>
+						<a href="#" class="search_close"><span class="icon-cancel-circled"></span></a>
+					</div>
 
-						</div>
+					<!-- main menu -->
+					<nav id="mainmenu_area" class="mainmenu_area theme_menu" role="navigation">
+					<ul id="mainmenu" class="sf-js-enabled">
+						<li class="lightblue"><a href="{{ route ('inicio') }} " class="sf-with-ul">Inicio<span class="sf-sub-indicator icon-right-open"></span></a></li>
+						<li class="brown"><a href="{{ route ('web') }} " class="sf-with-ul">Webs<span class="sf-sub-indicator icon-right-open"></span></a></li>
+						<li id="blob" style="width: 0px; height: 0px; left: 0px; top: 0px; background-color: rgb(187, 187, 187); opacity: 1;"></li>
+						<!--<li class="red"><a href="editarweb" class="sf-with-ul"> Editar webs <span class="sf-sub-indicator icon-right-open"></span></a></li>-->
+					</ul>
+					<a href="#" class="search_link"><span class="icon-search"></span></a>
+					</nav>
+					<!-- #main menu -->
+
 					</div>
 				</div>
-			<div id="header_middle_fixed"></div>
-		</header>
+			</div>
+		<div id="header_middle_fixed"></div>
+	</header>
 
-    @yield('contenido')
+	<!-- main -->
+		<div id="main" class="with_sidebar right_sidebar">
+		  <div id="main_inner" class="clearboth blog_style_excerpt">
+		    <!-- content -->
+		    <div id="content" class="content_blog" role="main" itemscope itemtype="http://schema.org/Blog">
+    			@yield('contenido')
+				</div>
+			</div>
+		</div>
+		<!-- /main -->
 
-		<!-- footer -->
-		<footer id="footer" class="site_footer" role="contentinfo">
-		<div id="footer_sidebar" class="widget_area theme_dark theme_article" role="complementary">
-		  <div id="footer_sidebar_inner">
-		    <!-- widget socials -->
-		    <aside class="widget widget_socials">
-		      <div class="widget_inner">
-		        <div class="logo logo_text"><a href="/">
-		          <span class="logo_title theme_header">top<span class="theme_accent">web</span>meetings</span></a>
-		        </div>
-		        <div class="logo_descr">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</div>
-		      </div>
-		    </aside>
-		    <!-- #widget socials -->
-		    <!-- widget flickr -->
-		    <aside class="widget widget_flickr">
-		      <h3 class="widget_title theme_title">Flickr</h3>
-		      <div class="flickr_images">
-		        <a href="#"><img title="" alt="" src=""></a>
-		      </div>
-		    </aside>
-		    <!-- widget #flickr -->
-		    <!-- widget recent post -->
-		    <aside class="widget widget_recent_entries widget-odd">
-		      <h3 class="widget_title theme_title">Recent posts</h3>
-		        <ul>
-		          <li><a href=""></a></li>
-		        </ul>
-		    </aside>
-		    <!-- #widget recent post -->
-		    <!-- widget contact form -->
-		    <aside class="widget widget_text">
-		      <h3 class="widget_title theme_title">Contact form</h3>
-		      <div class="textwidget">
-		        <div class="sc_contact_form">
-		          <form method="post">
-		            <div class="field field_name"><input type="text" id="nombre" name="username" placeholder="Nombre"></div>
-		            <div class="field field_email"><input type="text" id="emailFooter" name="email" value="<?php if(isset($_SESSION['email'])){ echo $email; }?>" <?php if(isset($_SESSION['email'])){ ?> readonly <?php } ?> placeholder="Email"></div>
-		            <div class="field field_message"><textarea id="mensaje" name="message" placeholder="Mensaje"></textarea></div>
-		            <button onclick="return contact()">Send</span></button>
-		            <div class="resultado_contact"></div>
+	<!-- footer -->
+	<footer id="footer" class="site_footer" role="contentinfo">
+	<div id="footer_sidebar" class="widget_area theme_dark theme_article" role="complementary">
+	  <div id="footer_sidebar_inner">
+	    <!--logo and description -->
+	    <aside class="widget widget_socials">
+	      <div class="widget_inner">
+	        <div class="logo logo_text"><a href="/">
+	          <span class="logo_title theme_header">top<span class="theme_accent">web</span>meetings</span></a>
+	        </div>
+	        <div class="logo_descr">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</div>
+	      </div>
+	    </aside>
+	    <!-- /logo and description -->
+	    <!-- widget flickr -->
+	    <aside class="widget widget_flickr">
+	      <h3 class="widget_title theme_title">Flickr</h3>
+	      <div class="flickr_images">
+	        <a href="#"><img title="" alt="" src=""></a>
+	      </div>
+	    </aside>
+	    <!-- widget #flickr -->
+	    <!-- widget recent post -->
+	    <aside class="widget widget_recent_entries widget-odd">
+	      <h3 class="widget_title theme_title">Recent posts</h3>
+	        <ul>
+	          <li><a href=""></a></li>
+	        </ul>
+	    </aside>
+	    <!-- #widget recent post -->
+	    <!-- widget contact form -->
+	    <aside class="widget widget_text">
+	      <h3 class="widget_title theme_title">Contact form</h3>
+	      <div class="textwidget">
+	        <div class="sc_contact_form">
+	          <form method="post">
+	            <div class="field field_name"><input type="text" id="nombre" name="username" placeholder="Nombre"></div>
+	            <div class="field field_email"><input type="text" id="emailFooter" name="email" value="<?php if(isset($_SESSION['email'])){ echo $email; }?>" <?php if(isset($_SESSION['email'])){ ?> readonly <?php } ?> placeholder="Email"></div>
+	            <div class="field field_message"><textarea id="mensaje" name="message" placeholder="Mensaje"></textarea></div>
+	            <button onclick="return contact()">Send</span></button>
+	            <div class="resultado_contact"></div>
 		          </form>
 		        </div>
 		      </div>
@@ -133,3 +149,75 @@
 		  </footer>
 		  <!-- #footer -->
 		</div>
+
+<script>
+function centrar() {
+    iz=(screen.width-document.body.clientWidth) / 2;
+    de=(screen.height-document.body.clientHeight) / 2;
+    moveTo(iz,de);
+}
+</script>
+<script type="text/javascript" src="js/scripts/registrarse.js?ver=<?= date('Y-m-d-h-i-s') ?>"></script>
+<!-- Login Acceder Form -->
+<a href="#" id="toTop" class="theme_button icon-up-open-big"></a>
+<div id="popup_login" class="popup_form">
+    <div class="popup_body theme_article">
+    <h4 class="popup_title">Login</h4>
+      <form action="Usuarios/login.php" method="post" name="login_form">
+        <div class="popup_field"><input type="text" name="log" id="log" placeholder="Login*" /></div>
+        <div class="popup_field"><input type="password" name="pwd" id="pwd" placeholder="Password*" /></div>
+        <div class="popup_field popup_button"><button href="#" type="submit" name="acceder" class="theme_button">Login</button></div>
+        <div class="popup_field forgot_password">
+          <a href="#">Forgot password?</a>
+        </div>
+      <div class="popup_field register">
+        <a href="#">Register</a>
+      </div>
+    </form>
+    </div>
+</div>
+<!-- /Login Acceder Form -->
+
+<div id="popup_register" class="popup_form">
+<div class="popup_body theme_article">
+<h4 class="popup_title">¡Regístrate!</h4>
+<div id="resultado_registrarse">HOLA </div>
+  <form action="registration_user" method="post" name="register_form">
+    <div class="popup_field"><input type="text" name="registration_username" id="registration_username" placeholder="Nombre (usuario)*" /></div>
+    <div class="popup_field"><input type="text" name="registration_email" id="registration_email" placeholder="Email*" /></div>
+    <div class="popup_field"><input type="password" name="registration_pwd" id="registration_pwd" placeholder="Contraseña*" /></div>
+    <div class="popup_field"><input type="password" name="registration_pwd2" id="registration_pwd2" placeholder="Confirma contraseña*" /><br><br></div>
+    <div class="popup_field theme_info registration_role"><h6>Política de privacidad:</h6>
+    <input type="radio" name="registration_role" id="registration_role2" value="1" checked="nochecked" />
+    <label class="lopd" for="registration_role2">He leído, entiendo y acepto la <a href="#">política de privacidad</a></label>
+    </div>
+    <div class="popup_field popup_button"><a onclick="return Registrarse()" href="#" class="theme_button">enviar</a></div>
+
+  </form>
+</div>
+</div>
+
+ <script type="text/javascript" src="/js/jquery/jquery.js"></script>
+ <script type="text/javascript" src="/js/jquery/jquery-migrate.min.js?ver2=<?= date('Ymdhis') ?>"></script>
+ <script type="text/javascript" src="/js/jquery/ui/jquery-ui-1.10.4.custom.js"></script>
+ <script type="text/javascript" src="/js/jquery.cookie.js"></script>
+ <script type="text/javascript" src="/js/jquery.easing.js"></script>
+ <script type="text/javascript" src="/js/superfish.js"></script>
+ <script type="text/javascript" src="/js/jquery.mobilemenu.min.js"></script>
+ <script type="text/javascript" src="/js/jquery.slidemenu.js"></script>
+ <script type="text/javascript" src="/js/skip-link-focus-fix.js"></script>
+ <script type="text/javascript" src="/js/_utils.js"></script>
+ <script type="text/javascript" src="/js/_googlemap_init.js"></script>
+ <script type="text/javascript" src="/js/_front.js?ver=<?= date('Ymdhis') ?>"></script>
+ <script type="text/javascript" src="/js/_reviews.js"></script>
+ <script type="text/javascript" src="/js/prettyphoto/jquery.prettyPhoto.min.js?ver=no-compose"></script>
+ <script type="text/javascript" src="/js/jquery.flexslider.min.js"></script>
+ <script type="text/javascript" src="/js/mediaplayer/mediaelement-and-player.min.js"></script>
+ <script type="text/javascript" src="/js/_contact_form.js"></script>
+ <script type="text/javascript" src="/js/jquery/ui/jquery.ui.core.min.js"></script>
+ <script type="text/javascript" src="/js/jquery/ui/jquery.ui.widget.min.js"></script>
+ <script type="text/javascript" src="/js/jquery/ui/jquery.ui.mouse.min.js"></script>
+ <script type="text/javascript" src="/js/jquery/ui/jquery.ui.draggable.min.js"></script>
+ <script type="text/javascript" src="/js/scripts/login.js"></script>
+</body>
+</html>
